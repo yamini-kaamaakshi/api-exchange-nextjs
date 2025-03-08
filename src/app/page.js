70c1b6f2994d306a9c3445f1fca48bb50f15df01
Yamini <yaminikaamaakshi@gmail.com>
2025-03-08 09:51:33 +0530

    Added chromeExtension file

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Features from "@/components/Features";
+import ChromeExtension from "@/components/ChromeExtension";
 
 export default function Home() {
     return (
         <>
             <Features/>
+            <ChromeExtension />
         </>
 
     );
