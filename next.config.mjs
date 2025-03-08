1ad73d2fe3bb089379166fca30e534ea94db0164
aruna <arunakamakshi09@gmail.com>
2025-03-08 09:40:48 +0530

    Updated the code.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn.apiexchange.site",
-            },
-        ],
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import Navbar from "@/components/Navigation";
 import Features from "@/components/Features";
-import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
         <>
-            <Navbar/>
             <Features/>
-            <Footer/>
         </>
 
     );
